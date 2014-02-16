2d562d37cbec0b99a0ceb391177e4aef531dc45b
John Ray Bautista <jrpbautista@gmail.com>
2014-02-16 13:19:38 +0800

    Update manage_account.php
    added ------ $user_email=$_POST["email"];
    ------------ mailer for approve request

--- controllers/manage_account.php
+++ controllers/manage_account.php
@@ -17,9 +17,42 @@ class Manage_account extends CI_Controller {
     }
 
     public function manipulate_account(){
+        //construct mailer
+        $email_config = Array(
+            'protocol'  => 'smtp',
+            'smtp_host' => 'ssl://smtp.googlemail.com',
+            'smtp_port' => 465,
+            'smtp_user' => 'ics.elib.admistrator@gmail.com',
+            'smtp_pass' => 'icselibadmin'
+        );
+        
+        $user_email = $_POST["email"];
+        
         if(isset($_POST["approve"])){
-            $this->manage_account_model->approve_account();
+            $this->load->library('email', $email_config);
+			$this->email->set_newline("\r\n");
+
+			$this->email->from('ics.elib.administrator@gmail.com', 'ICS e-lib Admistrator');
+			$this->email->to($user_email);
+			$this->email->subject('Request for an ICS e-Lib Account approved!');
+			$this->email->message("
+			Greetings from ICS e-Lib!
+
+			Your request has been approved. Check what's new in our e-Lib!
+			Thank You!
+					 
+			Yours Truly,
+			ICS e-Lib DevTeam
+			");
+
+				if( $this->email->send()){
+					$this->manage_account_model->approve_account();
+				}
+				else{
+					show_error($this->email->print_debugger());
+				}
         }
+        
         else if(isset($_POST["deactivate"])){
             $this->manage_account_model->deactivate_account();
         }
